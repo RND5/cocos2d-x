11b7cff90de32b7eee93adfd238bd5862acc49aa
jungtong <jungtong@idis.co.kr>
2014-01-24 16:52:34 +0900

    - Layout_Test 프로젝트 추

---
+++ projects/TEST_Layout/Classes/HelloWorldScene.cpp
@@ -0,0 +1,81 @@
+#include "HelloWorldScene.h"
+
+
+USING_NS_CC;
+USING_NS_CC_EXT;
+//using namespace cocos2d::extension;
+
+CCScene* HelloWorld::scene()
+{
+    // 'scene' is an autorelease object
+    CCScene *scene = CCScene::create();
+    
+    // 'layer' is an autorelease object
+    HelloWorld *layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !TouchGroup::init() )
+    {
+        return false;
+    }
+    
+    CCDirector::sharedDirector()->setRASDelegate(this);
+    
+    CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
+
+    this->setTag(3);
+    this->setColor(ccGREEN);
+    this->setOpacity(255);
+
+    
+    cocos2d::gui::Widget* pWidget = GUIReader::shareReader()->widgetFromJsonFile("test01_1.json");
+    this->addWidget(pWidget);
+    pWidget->setSize(ccp(visibleSize.width - 50, visibleSize.height-50)); // 현재의
+    
+    return true;
+}
+
+
+void HelloWorld::screenSizeChanged(float width, float height)
+{
+    CCEGLView::sharedOpenGLView()->setDesignResolutionSize(width, height, kResolutionShowAll);
+
+    CCScene* runScene = CCDirector::sharedDirector()->getRunningScene();
+    
+    if (runScene != NULL) {
+        TouchGroup* layer =  (TouchGroup*)runScene->getChildByTag(3);
+        layer -> setContentSize(CCSize(width, height));
+
+        cocos2d::gui::Widget* pWidget = layer->getWidgetByTag(10);
+        pWidget->setSize(CCSize(width - 50, height - 50));
+        pWidget->setPosition(ccp(25, 25));
+
+        cocos2d::gui::Widget* widgetBasePanel = layer->getWidgetByTag(20);
+        
+        widgetBasePanel -> setSize(CCSize(pWidget->getSize().width - 20, 100));
+        widgetBasePanel -> setPosition(ccp(10, pWidget->getSize().height - widgetBasePanel->getSize().height - 10));
+
+        
+        cocos2d::gui::Widget* widget1 = (cocos2d::gui::Widget*)layer->TouchGroup::getWidgetByTag(31);
+        cocos2d::gui::Widget* widget2 = (cocos2d::gui::Widget*)layer->TouchGroup::getWidgetByTag(32);
+
+        if (widget1 != NULL) {
+            widget1 -> setPosition(ccp(10, 10));
+        }
+        if (widget2 != NULL) {
+            widget2 -> setPosition(ccp(widgetBasePanel->getSize().width - 10 - widget2->getSize().width, widget1->getPositionY()));
+        }
+    }
+}
+
---
+++ projects/TEST_Layout/proj.wp8/TEST_Layout.cpp
@@ -0,0 +1,149 @@
+﻿#include <wrl/client.h>
+#include <d3d11_1.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <ppltasks.h>
+#include "TEST_Layout.h"
+#include "CCApplication.h"
+
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::System;
+using namespace Windows::Foundation;
+using namespace Windows::Graphics::Display;
+using namespace Windows::Phone::UI::Input;
+using namespace Windows::Graphics::Display;
+using namespace concurrency;
+USING_NS_CC;
+
+TEST_Layout::TEST_Layout()
+{
+}
+
+void TEST_Layout::Initialize(CoreApplicationView^ applicationView)
+{
+	applicationView->Activated +=
+		ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &TEST_Layout::OnActivated);
+
+	CoreApplication::Suspending +=
+		ref new EventHandler<SuspendingEventArgs^>(this, &TEST_Layout::OnSuspending);
+
+	CoreApplication::Resuming +=
+		ref new EventHandler<Platform::Object^>(this, &TEST_Layout::OnResuming);
+}
+
+void TEST_Layout::SetWindow(CoreWindow^ window)
+{
+    // Specify the orientation of your application here
+    // The choices are DisplayOrientations::Portrait or DisplayOrientations::Landscape or DisplayOrientations::LandscapeFlipped
+	DisplayProperties::AutoRotationPreferences = DisplayOrientations::None;
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &TEST_Layout::OnVisibilityChanged);
+
+	window->Closed += 
+		ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &TEST_Layout::OnWindowClosed);
+
+	window->PointerPressed +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &TEST_Layout::OnPointerPressed);
+
+	window->PointerMoved +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &TEST_Layout::OnPointerMoved);
+
+	window->PointerReleased +=
+		ref new TypedEventHandler<CoreWindow^, PointerEventArgs^>(this, &TEST_Layout::OnPointerReleased);
+
+    CCEGLView* eglView = new CCEGLView();
+	eglView->Create(window);
+    eglView->setViewName("TEST_Layout");
+}
+
+void TEST_Layout::Load(Platform::String^ entryPoint)
+{
+}
+
+void TEST_Layout::Run()
+{
+    CCApplication::sharedApplication()->run();
+}
+
+void TEST_Layout::Uninitialize()
+{
+}
+
+void TEST_Layout::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnVisibilityChanged(sender, args);
+}
+
+void TEST_Layout::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnWindowClosed(sender, args);
+}
+
+void TEST_Layout::OnPointerPressed(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerPressed(sender, args);
+}
+
+void TEST_Layout::OnPointerMoved(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerMoved(sender, args);
+}
+
+void TEST_Layout::OnPointerReleased(CoreWindow^ sender, PointerEventArgs^ args)
+{
+	CCEGLView::sharedOpenGLView()->OnPointerReleased(sender, args);
+}
+
+void TEST_Layout::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+	HardwareButtons::BackPressed += ref new EventHandler<BackPressedEventArgs^>(this, &TEST_Layout::OnBackButtonPressed);   
+	CoreWindow::GetForCurrentThread()->Activate();
+}
+
+void TEST_Layout::OnBackButtonPressed(Object^ sender, BackPressedEventArgs^ args)
+{
+    // Leave args->Handled set to false and the app will quit when user presses the back button on the phone
+}
+
+void TEST_Layout::OnSuspending(Platform::Object^ sender, SuspendingEventArgs^ args)
+{
+	// Save app state asynchronously after requesting a deferral. Holding a deferral
+	// indicates that the application is busy performing suspending operations. Be
+	// aware that a deferral may not be held indefinitely. After about five seconds,
+	// the app will be forced to exit.
+	SuspendingDeferral^ deferral = args->SuspendingOperation->GetDeferral();
+	//m_renderer->ReleaseResourcesForSuspending();
+
+	create_task([this, deferral]()
+	{
+		// Insert your code here.
+
+		deferral->Complete();
+	});
+}
+ 
+void TEST_Layout::OnResuming(Platform::Object^ sender, Platform::Object^ args)
+{
+	// Restore any data or state that was unloaded on suspend. By default, data
+	// and state are persisted when resuming from suspend. Note that this event
+	// does not occur if the app was previously terminated.
+	// m_renderer->CreateWindowSizeDependentResources();
+}
+
+IFrameworkView^ Direct3DApplicationSource::CreateView()
+{
+	return ref new TEST_Layout();
+}
+
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+	auto direct3DApplicationSource = ref new Direct3DApplicationSource();
+	CoreApplication::Run(direct3DApplicationSource);
+	return 0;
+}
